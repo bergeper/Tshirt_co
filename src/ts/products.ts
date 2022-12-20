fd898eb334efa80070ba831f611c618dc05475a3
Per Berge <berge.per2493@gmail.com>
2022-12-20 15:55:07 +0100

    Added image to loop in product page

--- src/ts/products.ts
+++ src/ts/products.ts
@@ -5,10 +5,15 @@ for (let i = 0; i < products.length; i++) {
   let clothingName: HTMLHeadingElement = document.createElement(
     "h2"
   ) as HTMLHeadingElement;
-  console.log(products[i].name);
+  let clothingImage: HTMLImageElement = document.createElement(
+    "img"
+  ) as HTMLImageElement;
 
   clothingName.innerHTML = products[i].name;
+  clothingImage.src = products[i].image;
+  clothingImage.alt = products[i].name;
 
   div.appendChild(clothingName);
+  div.appendChild(clothingImage);
   document.body.appendChild(div);
 }
