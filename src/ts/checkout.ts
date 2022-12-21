fdaa08b854e22c6478a4d22cb6ce1d2a65d2fc1d
MAC <miravitsas1@gmail.com>
2022-12-21 11:20:40 +0100

    fick min modal att funka

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -1,15 +1,17 @@
 let clickOnCart = document.getElementById("cart__icon") as HTMLButtonElement;
+clickOnCart.setAttribute("data-bs-toggle", "modal");
+clickOnCart.setAttribute("data-bs-target", "#exampleModal");
+
 clickOnCart.addEventListener("click", openModal); ///hämtar knappen och gör ett click-event.
 
 function openModal() {
-  let modalBody = document.getElementById("modal-body") as HTMLDivElement; ///hämtar modalbodyn från html
-  modalBody.innerHTML = " "; //tömmer inför varje gång
+  let modalContainer = document.getElementById("modal-body") as HTMLDivElement; ///hämtar modalbodyn från html
+  //modalContainer.innerHTML = " "; //tömmer inför varje gång
   //funktion för modalen.
   console.log("hejhej");
-  let modalContainer = document.createElement("div") as HTMLDivElement;
 
-  modalContainer.setAttribute("data-bs-toggle", "modal"); //sätter alla attribut som är utkommenderade i html
-  modalContainer.setAttribute("data-bs-target", "#exampleModal");
+  //modalContainer.setAttribute("data-bs-toggle", "modal"); //sätter alla attribut som är utkommenderade i html
+  //modalContainer.setAttribute("data-bs-target", "#exampleModal");
 
   let modalTitle = document.getElementById(
     //hämtar modaltiteln med id från html
@@ -18,4 +20,5 @@ function openModal() {
   modalTitle.innerHTML = "här sätter vi titel";
 
   document.body.appendChild(modalContainer);
+  modalContainer.appendChild(modalTitle);
 }
