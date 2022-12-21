112c145a27bf6c1217c21d9214a0033d93d430cd
MAC <miravitsas1@gmail.com>
2022-12-21 11:39:42 +0100

    exporterade cart.ts

--- src/ts/helpers/cart.ts
+++ src/ts/helpers/cart.ts
@@ -1,15 +1,18 @@
-// create Menu
-export const createCart = () => {
-  // menu
-  // TESTKÅD
-  const menuContainer: HTMLDivElement = document.querySelector(
-    ".cart__menu"
-  ) as HTMLDivElement;
+export function openCartModal() {
+  let modalContainer = document.getElementById("modal-body") as HTMLDivElement; ///hämtar modalbodyn från html
+  //modalContainer.innerHTML = " "; //tömmer inför varje gång
+  //funktion för modalen.
+  console.log("hejhej");
 
-  let isHidden = menuContainer.style.display === "none";
-  if (isHidden) {
-    menuContainer.style.display = "block";
-  } else {
-    menuContainer.style.display = "none";
-  }
-};
+  //modalContainer.setAttribute("data-bs-toggle", "modal"); //sätter alla attribut som är utkommenderade i html
+  //modalContainer.setAttribute("data-bs-target", "#exampleModal");
+
+  let modalTitle = document.getElementById(
+    //hämtar modaltiteln med id från html
+    "exampleModalLabel"
+  ) as HTMLHeadingElement;
+  modalTitle.innerHTML = "här sätter vi titel";
+
+  document.body.appendChild(modalContainer);
+  modalContainer.appendChild(modalTitle);
+}
