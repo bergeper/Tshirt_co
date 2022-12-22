3396069081dc21c596b354b3535565663c2281fd
Per Berge <berge.per2493@gmail.com>
2022-12-22 13:33:23 +0100

    Removed comments

--- src/ts/helpers/addToCart.ts
+++ src/ts/helpers/addToCart.ts
@@ -6,20 +6,3 @@ export function getFromLocalStorage() {
   );
   return objectsFromLS;
 }
-
-/*
-  for (let i = 0; i < objectsFromLS.length; i++) {
-    let cartObjectArticleId: number = objectsFromLS[i].articleId;
-    let cartObjectName: string = objectsFromLS[i].name;
-    let cartObjectImage: string = objectsFromLS[i].image;
-    let cartObjectPrice: number = objectsFromLS[i].price;
-    let cartObjectSize: string = objectsFromLS[i].size;
-    let newProductsInCart: ProductCart[] = [];
-    newProductsInCart = new ProductCart(cartObjectArticleId,cartObjectImage,cartObjectName,cartObjectPrice,cartObjectSize)
-    productsInCart
-   //newProductsInCart.push(cartObjectArticleId,cartObjectImage,c)
-   return productsInCart
-}
-//gör en ny tom lista, skicka in ohc returna
-//productsInCart
-*/
