13718b1cae4e80ca537d6c8f60d33a0fb6be7a24
Per Berge <berge.per2493@gmail.com>
2022-12-22 15:59:03 +0100

    added loop around

--- src/ts/helpers/cart.ts
+++ src/ts/helpers/cart.ts
@@ -13,55 +13,57 @@ export function buttonAttributes() {
 export function openCartModal() {
   cartProducts = getFromLocalStorage();
   console.log(cartProducts);
+  for (let i = 0; i < cartProducts.length; i++) {
+    let modalContainer = document.getElementById(
+      "modal-body"
+    ) as HTMLDivElement; //get modalbody from html
+    modalContainer.innerHTML = ""; //empty container
+    let modalTitle = document.getElementById(
+      "exampleModalLabel"
+    ) as HTMLHeadingElement;
+    modalTitle.innerHTML = "Varukorg";
 
-  let modalContainer = document.getElementById("modal-body") as HTMLDivElement; //get modalbody from html
-  modalContainer.innerHTML = ""; //empty container
-  let modalTitle = document.getElementById(
-    "exampleModalLabel"
-  ) as HTMLHeadingElement;
-  modalTitle.innerHTML = "Varukorg";
+    let productName: HTMLParagraphElement = document.createElement("p");
+    productName.className = "modal-body__priceName";
+    productName.innerHTML = "NAMNET:...";
 
-  let productName: HTMLParagraphElement = document.createElement("p");
-  productName.className = "modal-body__priceName";
-  productName.innerHTML = "NAMNET:...";
+    let productPrice: HTMLParagraphElement = document.createElement("p");
+    productPrice.className = "modal-body__priceText";
+    productPrice.innerHTML = "PRISET...";
 
-  let productPrice: HTMLParagraphElement = document.createElement("p");
-  productPrice.className = "modal-body__priceText";
-  productPrice.innerHTML = "PRISET...";
+    let cartImage: HTMLImageElement = document.createElement("img");
+    cartImage.className = "modal-body__cartImage";
+    cartImage.src =
+      "https://www.shirtstore.se/pub_images/original/15348_r_20268.jpg?extend=copy&width=1280&method=fit&height=1280&type=webp";
 
-  let cartImage: HTMLImageElement = document.createElement("img");
-  cartImage.className = "modal-body__cartImage";
-  cartImage.src =
-    "https://www.shirtstore.se/pub_images/original/15348_r_20268.jpg?extend=copy&width=1280&method=fit&height=1280&type=webp";
+    let cartQuantity: HTMLParagraphElement = document.createElement("p");
+    cartQuantity.innerHTML = "Antal:";
+    cartQuantity.className = "modal-body__cartQuantity";
+    let totalAmount: HTMLParagraphElement = document.createElement("p");
+    totalAmount.innerHTML = "Totalt Summa:....";
+    totalAmount.className = "modal-body__totalAmount";
 
-  let cartQuantity: HTMLParagraphElement = document.createElement("p");
-  cartQuantity.innerHTML = "Antal:";
-  cartQuantity.className = "modal-body__cartQuantity";
-  let totalAmount: HTMLParagraphElement = document.createElement("p");
-  totalAmount.innerHTML = "Totalt Summa:....";
-  totalAmount.className = "modal-body__totalAmount";
+    let quantityDiv: HTMLDivElement = document.createElement("div");
+    quantityDiv.className = "cartDiv__quantityDiv";
 
-  let quantityDiv: HTMLDivElement = document.createElement("div");
-  quantityDiv.className = "cartDiv__quantityDiv";
-
-  //create - + buttons
-  let removeButton: HTMLButtonElement = document.createElement("button");
-  let addButton: HTMLButtonElement = document.createElement("button");
-  removeButton.className = "modal-body__removeButton";
-  addButton.className = "modal-body__addButton";
-  removeButton.innerHTML = "-";
-  addButton.innerHTML = "+";
-  modalContainer.appendChild(productName);
-  modalContainer.appendChild(productPrice);
-  modalContainer.appendChild(cartImage);
-  modalContainer.appendChild(addButton);
-  modalContainer.appendChild(cartQuantity);
-  modalContainer.appendChild(removeButton);
-  modalContainer.appendChild(totalAmount);
-
+    //create - + buttons
+    let removeButton: HTMLButtonElement = document.createElement("button");
+    let addButton: HTMLButtonElement = document.createElement("button");
+    removeButton.className = "modal-body__removeButton";
+    addButton.className = "modal-body__addButton";
+    removeButton.innerHTML = "-";
+    addButton.innerHTML = "+";
+    modalContainer.appendChild(productName);
+    modalContainer.appendChild(productPrice);
+    modalContainer.appendChild(cartImage);
+    modalContainer.appendChild(addButton);
+    modalContainer.appendChild(cartQuantity);
+    modalContainer.appendChild(removeButton);
+    modalContainer.appendChild(totalAmount);
+  }
   ///console.log(cartAttributes);
   /*
-  for (let i = 0; i < productsInCart.length; i++) {
+      for (let i = 0; i < productsInCart.length; i++) {
     let cartArticleId: number = productsInCart[i].product;
     let cartImage: string = productsInCart[i].produc
     let cartProductName: string = productsInCart[i].;
