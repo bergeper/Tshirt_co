631e578de0678fc2eef97b194f4052224dedf186
Per Berge <berge.per2493@gmail.com>
2022-12-22 14:55:53 +0100

    WoopWoop

--- src/ts/helpers/cart.ts
+++ src/ts/helpers/cart.ts
@@ -56,6 +56,7 @@ export function openCartModal() {
   modalContainer.appendChild(removeButton);
   modalContainer.appendChild(totalAmount);
   /*
+  KAKA
   let productsInCart: ProductCart[] = getFromLocalStorage();
   ///console.log(cartAttributes);
   
