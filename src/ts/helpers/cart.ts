478621a3f16a2bd97ba2392b7a621aafd0f796be
MAC <miravitsas1@gmail.com>
2022-12-22 13:46:57 +0100

    ändring i cart

--- src/ts/helpers/cart.ts
+++ src/ts/helpers/cart.ts
@@ -12,53 +12,33 @@ export function openCartModal() {
   let modalContainer = document.getElementById("modal-body") as HTMLDivElement; //get modalbody from html
   modalContainer.innerHTML = ""; //empty container
   let modalTitle = document.getElementById(
-    //gets the modaltitle from html
     "exampleModalLabel"
   ) as HTMLHeadingElement;
   modalTitle.innerHTML = "Varukorg";
 
-  let cartHeading: HTMLHeadingElement = document.createElement("h5");
-  cartHeading.innerHTML = "Dina Varor:";
-
-  let informationDiv: HTMLDivElement = document.createElement("div");
-  informationDiv.className = "cartDiv";
-  informationDiv.innerHTML = "";
-
   let productName: HTMLParagraphElement = document.createElement("p");
-  productName.className = "priceName";
-  productName.innerHTML = "NAMNET";
+  productName.className = "modal-body__priceName";
+  productName.innerHTML = "NAMNET:...";
 
   let productPrice: HTMLParagraphElement = document.createElement("p");
-  productPrice.className = "priceText";
-  productPrice.innerHTML = "PRISET";
-
-  let productAttributes: HTMLDivElement = document.createElement("div");
-  productAttributes.className = "cartDiv__productAttributesDiv";
+  productPrice.className = "modal-body__priceText";
+  productPrice.innerHTML = "PRISET...";
 
   let cartImage: HTMLImageElement = document.createElement("img");
-  cartImage.className = "cartDiv__cartImage";
+  cartImage.className = "modal-body__cartImage";
   cartImage.src =
     "https://www.shirtstore.se/pub_images/original/15348_r_20268.jpg?extend=copy&width=1280&method=fit&height=1280&type=webp";
 
   let cartQuantity: HTMLParagraphElement = document.createElement("p");
   cartQuantity.innerHTML = "Antal:.........";
   let totalAmount: HTMLParagraphElement = document.createElement("p");
-  totalAmount.innerHTML = "Total Summa:........";
+  totalAmount.innerHTML = "Totalt Summa:........";
 
-  let quantityDiv: HTMLDivElement = document.createElement("div");
-  quantityDiv.className = "cartDiv__quantityDiv";
-
-  document.createElement;
-
-  productAttributes.appendChild(productName);
-  productAttributes.appendChild(productPrice);
-  modalContainer.appendChild(cartHeading);
-  modalContainer.appendChild(informationDiv);
-  informationDiv.appendChild(productAttributes);
-  informationDiv.appendChild(cartImage);
-  informationDiv.appendChild(quantityDiv);
-  quantityDiv.appendChild(cartQuantity);
-  quantityDiv.appendChild(totalAmount);
+  modalContainer.appendChild(productName);
+  modalContainer.appendChild(productPrice);
+  modalContainer.appendChild(cartImage);
+  modalContainer.appendChild(cartQuantity);
+  modalContainer.appendChild(totalAmount);
 
   let productsInCart: ProductCart[] = getFromLocalStorage();
   ///console.log(cartAttributes);
