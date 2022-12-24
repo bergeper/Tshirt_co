acf113af19fb15aa570c7e44c5eb82c044928e8e
Caroline Gryglas <Caroline@carolines-mbp.guest.huddinge.se>
2022-12-24 01:06:47 +0100

    function for totalsum

---
+++ src/ts/helpers/totalsum.ts
@@ -0,0 +1,10 @@
+/* function totalsum() {
+    let sum = 0;
+    let totalSum = document.getElementById("totalsum") as HTMLParagraphElement;
+    for (let i = 0; i < cartProductsFromLS.length; i++) {
+      console.log(cartProductsFromLS[i]);
+      sum += cartProductsFromLS[i].price * cartProductsFromLS[i].quantity;
+      totalSum.innerHTML = sum.toString();
+    }
+  }
+   */
