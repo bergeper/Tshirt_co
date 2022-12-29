92c36c0b9366e55af710ad3713b9b5f383bf1813
Per Berge <berge.per2493@gmail.com>
2022-12-29 17:48:26 +0100

    Added comments

--- src/ts/products.ts
+++ src/ts/products.ts
@@ -117,6 +117,7 @@ function cartProductToCart(cartProduct: Product) {
   // Creating a new object based on class ProductCart
   const cartProductToLS: ProductCart = new ProductCart(1, cartProduct);
 
+  // checking if list of products is empty.
   let found: boolean = false;
   if (cartProducts.length === 0) {
     found = false;
@@ -125,6 +126,7 @@ function cartProductToCart(cartProduct: Product) {
       if (
         cartProducts[i].product.articleId === cartProductToLS.product.articleId
       ) {
+        // changing the quantity instead of adding another of the same item.
         console.log("Om id hittas: ", cartProducts);
         cartProducts[i].quantity++;
         localStorage.setItem("Cart", JSON.stringify(cartProducts));
@@ -134,6 +136,7 @@ function cartProductToCart(cartProduct: Product) {
       }
     }
   }
+  // if the product does not exist in localStorage
   if (found === false) {
     console.log("Om id inte hittas: ", cartProducts);
     cartProducts.push(cartProductToLS);
